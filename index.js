e67125dc5f470ebe9b982289a4aa8f078d79ce05
void-ness <78547746+void-ness@users.noreply.github.com>
2023-09-01 15:34:13 +0530

    add: news endpoints

--- index.js
+++ index.js
@@ -1,32 +1,47 @@
-require('dotenv').config();
+require('dotenv').config(); // for loading env variables
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const axios = require('axios');
+const axios = require('axios'); // for making http requests
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const port = 3000;
-const apikey = process.env.API_KEY;
+const port = process.env.PORT || 3000;
+const apikey = process.env.API_KEY; // api key for the newsAPI
 
-app.get('/', async (req, res) => {
+// to get viral news on the basis of entered keywords or category wise
+app.get('/viralNews', async (req, res) => {
+    const query = req.query;
+    console.log(query);
+
     try {
-        const msg = await getNews();
-        // console.log(msg);
+        const msg = await getNews(query.keyword);
         res.status(200).json(msg);
-        // throw new Error("demo");
     }
 
     catch (error) {
         res.status(500).json({ error: 'failed to fetch data' });
     }
-
 });
 
-// function to fetch news of a particular topic
-async function getNews() {
+// to get viral headlines of a particular country
+app.get('/viralHeadlines', async (req, res) => {
+    const query = req.query;
+
+    try {
+        const msg = await getNewsHeadlines(query.country, query.page, query.pageSize);
+        res.status(200).json(msg);
+    }
+
+    catch (error) {
+        res.status(500).json({ error: 'failed to fetch data' });
+    }
+});
+
+// function to fetch news of a particular topic and category wise news
+async function getNews(keyword) {
     try {
         const params = {
             params: {
@@ -34,9 +49,9 @@ async function getNews() {
                 searchIn: 'title,description',
                 language: 'en',
                 page: 1,
-                pageSize: 1,
+                pageSize: 6,
                 apikey: apikey,
-                q: 'india'
+                q: keyword
             }
         }
 
@@ -47,7 +62,28 @@ async function getNews() {
     catch (error) {
         throw new Error("unable to fetch results from the API");
     }
+}
 
+// function to fetch headlines of a particular country
+async function getNewsHeadlines(countryName = "in", page = 1, pageSize = 6) {
+    try {
+        const params = {
+            params: {
+                sortBy: 'relevancy',
+                page: page,
+                pageSize: pageSize,
+                apikey: apikey,
+                country: countryName
+            }
+        }
+
+        let result = await axios.get(`https://newsapi.org/v2/top-headlines`, params);
+        return result.data?.articles;
+    }
+
+    catch (error) {
+        throw new Error("unable to fetch results from the API");
+    }
 }
 
 app.listen(port, () => {
